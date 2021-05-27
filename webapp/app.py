46e0fcd6e54891d31396800b29f1858529d6e80a
Jaime Vargas <32944542+jaimev408@users.noreply.github.com>
2021-05-27 15:01:39 -0700

    Add files via upload

--- webapp/app.py
+++ webapp/app.py
@@ -4,14 +4,16 @@ from dash.dependencies import Input, Output, State
 import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#Style for the button
 BUTTON_STYLE = {
     'textAlign':'center',
     }
 
-
+#application init
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
+#App UI layout code
 button = html.Div([html.Button('Start Process!', id='start'),], style=BUTTON_STYLE,)
 
 
@@ -19,7 +21,9 @@ app.layout = html.Div([html.Div(html.H3("Data Engineering Proof of Concept. Pres
                        button,
                        html.Div(id='tabs-example-content'),
                        ])
-
+#Callback function to process data
+#input: button click
+#Output: done html message to populate layout
 @app.callback(Output('tabs-example-content', 'children'),
               Input('start', 'n_clicks'),)
 def startProcess(n_clicks):
@@ -31,7 +35,8 @@ def startProcess(n_clicks):
     toBucket(m, 'meanReviewsBus.csv')
     toBucket(z, 'mostActiveUsers.csv')
     toBucket(u, 'topZipReviews.csv')
+    return html.Div(html.H2("DONE"))
     
-
+#Main function to run app
 if __name__ == '__main__':
     app.run_server(debug=False)
